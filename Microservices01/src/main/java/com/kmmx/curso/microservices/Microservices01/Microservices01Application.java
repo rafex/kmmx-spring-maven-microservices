7bff0bdc495afc3ed7d7b9aaaa97912588931bc8
Raúl González <rafex@rafex.dev>
2021-06-14 19:48:32 -0500

    init

---
+++ Microservices01/src/main/java/com/kmmx/curso/microservices/Microservices01/Microservices01Application.java
@@ -0,0 +1,13 @@
+package com.kmmx.curso.microservices.Microservices01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices01Application.class, args);
+	}
+
+}
